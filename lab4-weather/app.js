e9f8fb88a53eb3051600fdd75842af04f4d1903b
Amber <amber.waltens@hotmail.com>
2020-03-19 18:02:06 +0100

    video Weather API + Geolocation  code added

---
+++ lab4-weather/app.js
@@ -0,0 +1,45 @@
+class App{
+    constructor(){
+        this.getLocation();
+        this.lat;
+        this.lng;
+    }
+
+    getLocation(){
+        navigator.geolocation.getCurrentPosition(
+        this.gotLocation.bind(this),
+        this.errorLocation.bind(this)
+        );
+    }
+
+    gotLocation(result){
+        //console.log(result);
+        this.lat = result.coords.latitude;
+        this.lng = result.coords.longitude;
+        //console.log(this.lat);
+        this.getWeather();
+    }
+
+    getWeather(){
+        //https://api.darksky.net/forecast/c1e93f827bcaf164be0e1e36ed5091c5/37.8267,-122.4233
+        const key = "c1e93f827bcaf164be0e1e36ed5091c5";
+        const cors = "https://cors-anywhere.herokuapp.com/";
+        let url = `${cors}https://api.darksky.net/forecast/${key}/${this.lat},${this.lng}?units=si`
+        fetch(url)
+            .then(response =>{
+                //console.log(response);
+               return response.json();
+            }).then(data =>{
+                //console.log(data);
+                document.querySelector("#weather").innerHTML = data.currently.summary;
+            }).catch(err =>{
+                console.log(err);
+            });
+    }
+
+    errorLocation(err){
+        console.log(err);
+    }
+}
+
+let app = new App();
