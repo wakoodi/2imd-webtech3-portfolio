aacc728b3dabec8aebc604640c1b66797a7f6054
Amber <amber.waltens@hotmail.com>
2020-03-26 12:16:41 +0100

    routes added

--- lab5/index.js
+++ lab5/index.js
@@ -7,13 +7,22 @@ app.get('/', (req, res) => {
 })
 
 //GET /api/v1/messages
+app.get('/api/v1/messages', (req, res) => {
+    res.send('GETTING messages')
+})
 
 //GET /api/v1/messages/:id
 
+
 //POST /api/v1/messages
+app.post('/api/v1/messages', (req, res) => {
+    res.send('POST messages')
+})
 
 //PUT /api/v1/messages/:id
-
+app.put('/api/v1/messages/:id', (req, res) => {
+    res.send('PUT messages' + req.params.id)
+})
 //DELETE /api/v1/messages/:id
 
 //GET /api/v1/messages?user=username
